b3123285cf2d442b76a6dfd42407b52587942007
iamtreetop <trivta@gmail.com>
2020-11-03 13:31:44 -0800

    Add Github and LinkedIn icons

--- dist/bundle.js
+++ dist/bundle.js
@@ -5,32 +5,39 @@
 /*! unknown exports (runtime-defined) */
 /*! runtime requirements:  */
 // const Game = require("./game")
+// import Background from "./background";
 
 document.addEventListener("DOMContentLoaded", () =>{
-    const canvas = document.querySelector('canvas')
+    const canvasEl = document.querySelector('canvas')
+    const ctx = canvasEl.getContext('2d');
+    const backgroundEl = document.getElementById("background");
     
-    canvas.width = 600;
-    canvas.height = 600;
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    const grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+    grd.addColorStop(0, '#8ED6FF');
+    grd.addColorStop(1, '#004CB3');
+    ctx.fillStyle = grd;
+    ctx.fill();
 
-    const ctx = canvas.getContext("2d");
-    // ctx.fillStyle = "#602b76"
-    // ctx.fillRect(100, 50, 100, 30);
-    // ctx.fillStyle = "#402a76"
-    // ctx.fillRect(150, 100, 160, 80);
-    // ctx.fillRect(250, 200, 100, 120);
-
-    // ctx.moveTo(50, 300);
-    // ctx.lineTo(300, 100);
-    // ctx.stroke();
     ctx.beginPath();
-    ctx.arc(300, 300, 150, 0, Math.PI * 2, false);
+    ctx.arc(250, 250, 150, 0, Math.PI * 2, false);
     ctx.strokeStyle = "lightgrey"
-    ctx.fillstyle = "burgundy"
     ctx.stroke();
 
-    // const canvas = document.getElementById("game-canvas")
+    for(let i = 0; i < 20; i++) {
+        let x = Math.random() * 250;
+        let y = Math.random() * 250;
+
+        ctx.beginPath();
+        ctx.arc(x, y, 20, 0, Math.PI * 2, false);
+        ctx.strokeStyle = "blue"
+        ctx.stroke();
+    }
+
     console.log("Webpack is working!")
 })
+
+
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
