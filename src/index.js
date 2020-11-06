962d7cdd49c93d5b770c0f9991fe6963eca2e55e
iamtreetop <trivta@gmail.com>
2020-11-06 00:01:25 -0800

    Add Projectile pause at rim

--- src/index.js
+++ src/index.js
@@ -30,6 +30,10 @@ document.addEventListener("DOMContentLoaded", () =>{
             ctx.fillStyle = this.color
             ctx.fill();
         }
+
+        update() {
+            this.draw()
+        }
     }
 
     class Projectile {
@@ -62,44 +66,90 @@ document.addEventListener("DOMContentLoaded", () =>{
     const player = new Player(x, y, 20, playerColor);
     const projectiles = []
 
+    function distance(x1, y1, x2, y2) {
+        let xDist = x2 - x1;
+        let yDist = y2 - y1;
+        return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+    }
+
+    let circle1;
+    let circle2;
+    function init(){
+        circle1 = new Player(300, 250, 130, 'transparent');
+        // circle2 = new Player(10, 10, 20, 'red')
+    }
+
+    let projectile;
     let animationId;
     function animate(){ 
-            animationId = requestAnimationFrame(animate);
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        animationId = requestAnimationFrame(animate);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
             
-            ctx.rect(0, 0, canvas.width, canvas.height);
-            const grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-            grd.addColorStop(0, '#8ED6FF');
-            grd.addColorStop(1, '#004CB3');
-            ctx.fillStyle = grd;
-            ctx.fill();
+        circle1.update();
+        // circle2.x = mouse.x;
+        // circle2.y = mouse.y;
+        // circle2.update();
+        ctx.rect(0, 0, canvas.width, canvas.height);
+        const grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+        grd.addColorStop(0, '#8ED6FF');
+        grd.addColorStop(1, '#004CB3');
+        ctx.fillStyle = grd;
+        ctx.fill();
 
-            ctx.beginPath();
-            ctx.arc(canvas.width / 2, canvas.height / 2, 175, 0, Math.PI * 2, false);
-            ctx.strokeStyle = "lightgrey"
-            ctx.stroke(); 
+        ctx.beginPath();
+        ctx.arc(canvas.width / 2, canvas.height / 2, 175, 0, Math.PI * 2, false);
+        ctx.strokeStyle = "lightgrey"
+        ctx.stroke(); 
             
-            player.draw();
-            projectiles.forEach(projectile => {
-            projectile.update();
+        player.draw();
+        projectiles.forEach(projectile => {
+        projectile.draw();
+        projectile.update();
+            
+            if (distance(circle1.x, circle1.y, projectile.x, projectile.y) > circle1.radius + projectile.radius) {
+                // make circle stop, not disappear
+                // projectile.x = x;
+                // projectile.y = y;
+                projectile.velocity = {
+                    x: 0,
+                    y: 0
+                };
+                // circle1.color = 'lightgrey';
+    
+            } else {
+                circle1.color = 'transparent';
+            }
+
         })
-        // projectile.draw();
         // projectile.update();
+        console.log(distance(circle1.x, circle1.y, player.x, player.y));
         // console.log('animate');
     }
+
+    const mouse = {
+        x: 10,
+        y: 10
+    }
+
+    addEventListener('mousemove', (event) => {
+        mouse.x = event.offsetX
+        mouse.y = event.offsetY
+    })
 
     addEventListener('click', (event) => {
         const angle = Math.atan2(event.offsetY - canvas.height / 2, event.offsetX- canvas.width / 2)
         const velocity = {
-            x: Math.cos(angle) * 5,
-            y: Math.sin(angle) * 5
+            x: Math.cos(angle) * 8,
+            y: Math.sin(angle) * 8
         }
+
         const projColor = `hsl(${Math.random() * 360}, 50%, 50%)`
         projectiles.push(
             new Projectile(canvas.width / 2, canvas.height / 2, 20, projColor, velocity)
         )
         console.log(event)
     })
+    init();
     animate();
 
     // ctx.rect(0, 0, canvas.width, canvas.height);
